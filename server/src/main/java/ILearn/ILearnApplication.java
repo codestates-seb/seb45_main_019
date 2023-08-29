8e79a2657fa35912e5d101dacc0f289d9270446c
hyeonwoxo <icc99031@gmail.com>
2023-08-29 13:25:29 +0900

    server first init

---
+++ server/src/main/java/ILearn/ILearnApplication.java
@@ -0,0 +1,13 @@
+package ILearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILearnApplication.class, args);
+	}
+
+}
