09fbaeed9959e6a5263f401b330583ab08eaccf8
kimsihyoung <rjswmtlgud@naver.com>
2023-08-29 14:16:50 +0900

    [FEAT]Member Entity

---
+++ server/src/main/java/ILearn/member/entity/Member.java
@@ -0,0 +1,62 @@
+package ILearn.member.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "registration_date")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date registrationDate;
+
+    @Column(name = "point")
+    private int point;
+
+    @Column(name = "member_status")
+    private String memberStatus;
+
+    @Column(name = "word_book", columnDefinition = "TEXT")
+    private String wordBook;
+
+//    @OneToMany
+//    @JoinColumn(name = "word_Id")
+//    private List<Word> words;
+//
+//    @OneToMany
+//    @JoinColumn(name = "learning_Id")
+//    private List<Learning> learnings;
+//
+//    @OneToMany
+//    @JoinColumn(name = "question_Id")
+//    private List<Question> questions;
+//
+//    @OneToOne
+//    @JoinColumn(name = "manage_Id")
+//    private Manage manage;
+
+}
---
+++ server/src/main/java/ILearn/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package ILearn.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//@Repository
+//public class MemberRepository extends JpaRepository<Member, Long> {
+
+//}
