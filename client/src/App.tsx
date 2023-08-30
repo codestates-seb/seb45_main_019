def181d99a9c36e672f1c084bd8044e1db04c691
Moses-Yu <mosesyumss@gmail.com>
2023-08-30 17:43:05 +0900

    [feat] SignUp 페이지 router에 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './pages/Main/MainPage';
+import SignUp from './pages/SignUp/SignUp';
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/signup" element={<SignUp></SignUp>} />
     </Routes>
   );
 }
