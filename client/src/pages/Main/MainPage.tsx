4f76152124491f6e45435120d16db751e2973cc4
Moses-Yu <mosesyumss@gmail.com>
2023-08-30 13:37:06 +0900

    [comment] 주석 제거

--- client/src/pages/Main/MainPage.tsx
+++ client/src/pages/Main/MainPage.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
-// import { useAppSelector, useAppDispatch } from '../../redux/hooks';
-// import { setUser } from '../../redux/slices/user';
 
 function MainPage() {
-  // const user = useAppSelector((state) => state.user);
-  // const { email, username, user_Id, nickname, point, memberStatus } =
-  //   useAppSelector((state) => state.user);
-  // const dispatch = useAppDispatch();
-  // dispatch(
-  //   setUser({
-  //     email: '11234',
-  //     username: '1234',
-  //     user_Id: 1234,
-  //     nickname: '1234',
-  //     point: 1234,
-  //     memberStatus: false
-  //   })
-  // );
-
   return (
     <div>
       <h1>I Learn</h1>
