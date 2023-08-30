7bb36893e8bbf2b624aa67f96439db1d43392604
Moses-Yu <mosesyumss@gmail.com>
2023-08-30 12:15:17 +0900

    [feat] 전역 상태 user 생성

--- client/src/pages/Main/MainPage.tsx
+++ client/src/pages/Main/MainPage.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { useAppSelector, useAppDispatch } from '../../redux/hooks';
-import { setUser } from '../../redux/slices/user';
+// import { useAppSelector, useAppDispatch } from '../../redux/hooks';
+// import { setUser } from '../../redux/slices/user';
 
 function MainPage() {
-  const user = useAppSelector((state) => state.user);
-  const dispatch = useAppDispatch();
+  // const user = useAppSelector((state) => state.user);
+  // const { email, username, user_Id, nickname, point, memberStatus } =
+  //   useAppSelector((state) => state.user);
+  // const dispatch = useAppDispatch();
+  // dispatch(
+  //   setUser({
+  //     email: '11234',
+  //     username: '1234',
+  //     user_Id: 1234,
+  //     nickname: '1234',
+  //     point: 1234,
+  //     memberStatus: false
+  //   })
+  // );
 
   return (
     <div>
       <h1>I Learn</h1>
-      <div>{user.email}</div>
-      <div>{user.memberId}</div>
-      <div>{user.name}</div>
-      <button
-        onClick={() =>
-          dispatch(setUser({ email: 'moses', name: 'moses', memberId: 10 }))
-        }
-      >setUser</button>
     </div>
   );
 }
--- client/src/redux/slices/user.ts
+++ client/src/redux/slices/user.ts
@@ -3,24 +3,33 @@ import { RootState } from '../store';
 
 export interface UserState {
   email: string;
-  name: string;
-  memberId: number;
+  username: string;
+  user_Id: number;
+  nickname: string;
+  point: number;
+  memberStatus: boolean;
 }
 
 const initialState: UserState = {
   email: '',
-  name: '',
-  memberId: 1
+  username: '',
+  user_Id: 0,
+  nickname: '',
+  point: 0,
+  memberStatus: false
 };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUser: (state, action) => {
+    setUser: (state, action: { payload: UserState }) => {
       state.email = action.payload.email;
-      state.name = action.payload.name;
-      state.memberId = action.payload.memberId;
+      state.username = action.payload.username;
+      state.user_Id = action.payload.user_Id;
+      state.nickname = action.payload.nickname;
+      state.point = action.payload.point;
+      state.memberStatus = action.payload.memberStatus;
       return state;
     }
   }
