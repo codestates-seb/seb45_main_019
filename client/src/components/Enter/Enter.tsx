e3be5812e2ed46e10232b95176b06571f40a46d1
k-impossible <kth3349@gmail.com>
2023-09-01 12:04:31 +0900

    [design] Enter 컴포넌트 스타일 작업 완료

--- client/src/components/Enter/Enter.tsx
+++ client/src/components/Enter/Enter.tsx
@@ -1,18 +1,49 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Button, Container, Typography } from '@mui/material';
 
 function Enter() {
   const imgSrc = `images/chapter${1}.png`;
   return (
-    <Container>
+    <Container
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
       <Box>
-        <Typography variant="h4" align="center" marginBottom={1}>
+        <Typography
+          variant="h4"
+          align="center"
+          marginBottom={1}
+          fontWeight={600}
+        >
           Chapter 1
         </Typography>
         <Typography paragraph align="center" color="text.secondary">
           인사나누기
         </Typography>
-        <Box>
-          <img src={imgSrc} alt="enterImg" />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 10
+          }}
+        >
+          <img
+            src={imgSrc}
+            style={{ width: '400px', height: '370px' }}
+            alt="enterImg"
+          />
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            sx={{ marginTop: 10 }}
+          >
+            학습하기
+          </Button>
         </Box>
       </Box>
     </Container>
