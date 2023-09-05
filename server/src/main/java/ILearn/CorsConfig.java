fa815bf4fa288842b9652bad4d749edc2426fe27
hyeonwoxo <icc99031@gmail.com>
2023-09-05 15:06:26 +0900

    [fix] cors 테스트 권한 수정

--- server/src/main/java/ILearn/CorsConfig.java
+++ server/src/main/java/ILearn/CorsConfig.java
@@ -13,10 +13,10 @@ public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("")); // 모든 도메인 허용
-        configuration.setAllowedMethods(Arrays.asList("")); // 모든 메서드 허용
-        configuration.setAllowedHeaders(Arrays.asList("")); // 모든 헤더 허용
-        configuration.setExposedHeaders(Arrays.asList("")); // 모든 노출 헤더 허용
+        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // 모든 도메인 허용
+        configuration.setAllowedMethods(Arrays.asList("*")); // 모든 메서드 허용
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
+        configuration.setExposedHeaders(Arrays.asList("*")); // 모든 노출 헤더 허용
         configuration.setAllowCredentials(true); // Credential 허용
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
