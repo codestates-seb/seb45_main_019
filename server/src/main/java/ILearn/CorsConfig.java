cae3441f4ae076e1c4db91cc1c3d58991dc199e8
hyeonwoxo <icc99031@gmail.com>
2023-09-05 15:16:10 +0900

    [fix] cors 테스트 권한 수정3

--- server/src/main/java/ILearn/CorsConfig.java
+++ server/src/main/java/ILearn/CorsConfig.java
@@ -6,12 +6,13 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
