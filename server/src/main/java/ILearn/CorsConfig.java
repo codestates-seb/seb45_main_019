648737afbc227d0e54742dd8675d0d45390f93eb
hyeonwoxo <icc99031@gmail.com>
2023-09-05 15:14:48 +0900

    [fix] cors 테스트 권한 수정2

--- server/src/main/java/ILearn/CorsConfig.java
+++ server/src/main/java/ILearn/CorsConfig.java
@@ -5,15 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // 모든 도메인 허용
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://i-learn.s3-website.ap-northeast-2.amazonaws.com/")); // 모든 도메인 허용
         configuration.setAllowedMethods(Arrays.asList("*")); // 모든 메서드 허용
         configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
         configuration.setExposedHeaders(Arrays.asList("*")); // 모든 노출 헤더 허용
