6a9e4d46cb09e3abe6a295eebe33c00101c4b0d0
Moses-Yu <mosesyumss@gmail.com>
2023-09-07 17:05:47 +0900

    [feat] voice load fix

--- client/src/components/Speaker/Speaker.tsx
+++ client/src/components/Speaker/Speaker.tsx
@@ -1,32 +1,32 @@
 import { IconButton } from '@mui/material';
 import RecordVoiceOverIcon from '@mui/icons-material/RecordVoiceOver';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Speaker(props: { text: string }) {
+  const [voiceloaded, setVoiceLoaded] = useState(false);
   const synth = window.speechSynthesis;
   const utterance = new SpeechSynthesisUtterance();
   utterance.text = props.text;
-  let desiredVoiceName = '';
-  // Get the user agent string
-  const userAgent = window.navigator.userAgent;
+  const desiredVoiceName =
+    'Microsoft Christopher Online (Natural) - English (United States)';
 
-  // Check if the user agent string contains "Macintosh" (for macOS) or "Windows" (for Windows)
-  if (userAgent.includes('Macintosh') || /iPhone|iPad/.test(userAgent)) {
-    // It's macOS
-    desiredVoiceName = 'Google UK English Female';
-  } else if (userAgent.includes('Windows') || /Android/.test(userAgent)) {
-    // It's Windows
-    desiredVoiceName =
-      'Microsoft Christopher Online (Natural) - English (United States)';
-  } else {
-    // It's neither macOS nor Windows (could be Linux or something else)
-    console.log('This computer is running an unknown operating system.');
-  }
+  // Get the user agent string
+  // const userAgent = window.navigator.userAgent;
+  // // Check if the user agent string contains "Macintosh" (for macOS) or "Windows" (for Windows)
+  // if (userAgent.includes('Macintosh') || /iPhone|iPad/.test(userAgent)) {
+  //   // It's macOS
+  //   desiredVoiceName =
+  //     'Microsoft Christopher Online (Natural) - English (United States)';
+  // } else if (userAgent.includes('Windows') || /Android/.test(userAgent)) {
+  //   // It's Windows
+  //   desiredVoiceName =
+  //     'Microsoft Christopher Online (Natural) - English (United States)';
+  // } else {
+  //   // It's neither macOS nor Windows (could be Linux or something else)
+  //   console.log('This computer is running an unknown operating system.');
+  // }
 
   // Find the desired voice by name
-  const voices = synth.getVoices();
-  console.log(voices);
-
   const desiredVoice = synth
     .getVoices()
     .find((voice) => voice.name === desiredVoiceName);
@@ -35,8 +35,14 @@ export default function Speaker(props: { text: string }) {
     utterance.voice = desiredVoice;
   }
 
+  synth.onvoiceschanged = () => {
+    setVoiceLoaded(true);
+  };
+
   const playText = () => {
-    synth.speak(utterance);
+    if (voiceloaded) {
+      synth.speak(utterance);
+    }
   };
   return (
     <IconButton onClick={playText}>
