fa82eafc8ea1e627afdad400bf167fae5bad7b5d
k-impossible <kth3349@gmail.com>
2023-09-12 17:27:46 +0900

    [test] 배포 테스트

--- client/src/interfaces/Learning.interface.ts
+++ client/src/interfaces/Learning.interface.ts
@@ -1,6 +1,6 @@
 export interface Learning {
   questionNum: number;
-  questionType: number;
+  questionType: number; //
   question: string; // 뜻 또는 단어
   examples: string[]; // 예시 선택지
   correct: number; // examples의 정답인 단어의 인덱스
