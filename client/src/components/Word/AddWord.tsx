4b15265605eb05383508629d07f10c24a9eaa846
Moses-Yu <mosesyumss@gmail.com>
2023-09-13 16:09:04 +0900

    [feat] 단어장 추가, 삭제 컴포넌트 AddWord 구현 완료

--- client/src/components/Word/AddWord.tsx
+++ client/src/components/Word/AddWord.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@mui/material';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import api from '../../common/utils/api';
@@ -7,24 +7,53 @@ import { AxiosError } from 'axios';
 
 export default function AddWord(props: { wordId: number }) {
   const queryClient = useQueryClient();
+
   const user = useAppSelector((state) => state.user);
-  console.log(user.userId);
 
   const { isLoading, error, data } = useQuery({
     queryKey: ['userWordIds'],
     queryFn: () => api(`/words/members/${user.userId}`).then(({ data }) => data)
   });
-  console.log(data);
 
-  const { mutate } = useMutation(
+  const [wordInWords, setWordInWords] = useState(false);
+  useEffect(() => {
+    if (user.memberStatus)
+      data.wordIds.includes(props.wordId)
+        ? setWordInWords(true)
+        : setWordInWords(false);
+  }, [data]);
+
+  const addToMyWords = useMutation(
     () =>
       api(`/words/members/${user.userId}`, 'post', {
+        wordId: props.wordId
+      }).then(({ data }) => {
+        console.log(data);
+      }),
+    {
+      onSuccess: () => queryClient.invalidateQueries(['userWordIds'])
+    }
+  );
+
+  const deleteFromMyWords = useMutation(
+    () =>
+      api(`/words/members/${user.userId}`, 'delete', {
         wordId: props.wordId
       }).then((res) => console.log(res.data)),
     {
       onSuccess: () => queryClient.invalidateQueries(['userWordIds'])
     }
   );
+
+  const handeClick = () => {
+    if (user.memberStatus)
+      if (wordInWords) {
+        deleteFromMyWords.mutate();
+      } else {
+        addToMyWords.mutate();
+      }
+    else alert('로그인 후 이용하실 수 있습니다.');
+  };
 
   if (isLoading) return <div> 로딩중... </div>;
 
@@ -33,8 +62,11 @@ export default function AddWord(props: { wordId: number }) {
     return <div> 에러: {myError.message} </div>;
   }
   return (
-    <Button variant={'outlined'} onClick={() => mutate()}>
-      단어장 추가
+    <Button
+      variant={wordInWords ? 'outlined' : 'contained'}
+      onClick={handeClick}
+    >
+      {wordInWords ? '단어장 해제' : '단어장 추가'}
     </Button>
   );
 }
