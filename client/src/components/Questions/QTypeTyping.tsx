e04d86832b059a8cd07d0fbc20c86ad44ca655c7
Moses-Yu <mosesyumss@gmail.com>
2023-09-16 23:11:45 +0900

    [feat] QTypeTyping 틀 구현

--- client/src/components/Questions/QTypeTyping.tsx
+++ client/src/components/Questions/QTypeTyping.tsx
@@ -1,5 +1,6 @@
-import { Box, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import { QTypeProps } from './QTypeChoice';
+import Speaker from '../Speaker/Speaker';
 
 export default function QTypeTyping(props: QTypeProps) {
   const question = props.question;
@@ -7,9 +8,48 @@ export default function QTypeTyping(props: QTypeProps) {
   const status = props.status;
   return (
     <Box sx={{ mt: 10 }}>
-      <Typography></Typography>
-      <Box sx={{ display: 'flex' }}>
-        <TextField sx={{ width: '4rem' }}></TextField>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          음성을 듣고 단어를 입력하세요.
+        </Typography>
+        {/* 음성 출력 버튼 */}
+        <Box>
+          <Speaker
+            text={`${question.question}'.  '${question.question}'.  '${question.question}`}
+          ></Speaker>
+        </Box>
+        {/* 답 입력 공간 */}
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <TextField
+            sx={{ width: '15rem', fontSize: '3rem' }}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              return handleChange(e);
+            }}
+            variant="outlined"
+            label="영어로 입력하세요."
+            margin="normal"
+            required
+            type="text"
+          ></TextField>
+        </Box>
+        {/* 확인 버튼 */}
+        <Box>
+          <Button variant={'outlined'}>확인</Button>
+        </Box>
       </Box>
     </Box>
   );
