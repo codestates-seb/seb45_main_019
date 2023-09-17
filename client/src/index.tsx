5b6dc4289ae8aebcdbfaf9d81a6aacfb9349fa78
Moses-Yu <mosesyumss@gmail.com>
2023-09-17 09:52:24 +0900

    [feat] redux-persist 적용

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import { GlobalStyle } from './style/Global.styled';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const queryClient = new QueryClient();
 const defaultTheme = createTheme();
@@ -18,7 +19,9 @@ root.render(
       <GlobalStyle />
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-          <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </Provider>
       </QueryClientProvider>
     </ThemeProvider>
