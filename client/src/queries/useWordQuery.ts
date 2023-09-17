58522dc0361012971c31efc0cfdac637d680a2ef
Moses-Yu <mosesyumss@gmail.com>
2023-09-17 13:54:42 +0900

    [fix] 오류 해결

---
+++ client/src/queries/useWordQuery.ts
@@ -0,0 +1,18 @@
+import api from '../common/utils/api';
+import { useQuery } from '@tanstack/react-query';
+import { WordInterface } from '../interfaces/Word.interface';
+
+const uri = '/words';
+
+export const getWordQueryKey = (id: number) => [uri, id];
+
+export const useWordQuery = (query: (string | number)[]) => {
+  return useQuery({
+    queryKey: query,
+    queryFn: () =>
+      api(`${query[0]}/${String(query[1])}`).then(
+        ({ data }) => data.data as WordInterface
+      ),
+    onError: (error) => error
+  });
+};
