f1c84307e7c41cbb906d1bf9dd7fd7d55549c853
Moses-Yu <mosesyumss@gmail.com>
2023-09-19 12:26:12 +0900

    [fix] speechSynthesis 원하는 목소리 리스트 순차적으로 검색하여 없을 경우에는 언어만 선택

--- client/src/common/utils/speak.ts
+++ client/src/common/utils/speak.ts
@@ -1,49 +1,50 @@
 const synth = window.speechSynthesis;
 const utterance = new SpeechSynthesisUtterance();
 
-function getBrowserInfo() {
-  // Get the user agent string
-  const userAgent = navigator.userAgent;
+// function getBrowserInfo() {
+//   // Get the user agent string
+//   const userAgent = navigator.userAgent;
 
-  // Define an array of known browser names and their corresponding regular expressions
-  const browsers = [
-    { name: 'Chrome', regex: /Chrome\/(\d+)/ },
-    { name: 'Firefox', regex: /Firefox\/(\d+)/ },
-    { name: 'Edge', regex: /Edge\/(\d+)/ },
-    { name: 'Internet Explorer', regex: /Trident\/\d+\.\d+;.*rv:(\d+)/ },
-    { name: 'Safari', regex: /Safari\/(\d+)/ }
-  ];
+//   // Define an array of known browser names and their corresponding regular expressions
+//   const browsers = [
+//     { name: 'Chrome', regex: /Chrome\/(\d+)/ },
+//     { name: 'Firefox', regex: /Firefox\/(\d+)/ },
+//     { name: 'Edge', regex: /Edge\/(\d+)/ },
+//     { name: 'Internet Explorer', regex: /Trident\/\d+\.\d+;.*rv:(\d+)/ },
+//     { name: 'Safari', regex: /Safari\/(\d+)/ }
+//   ];
 
-  // Iterate through the array to find a match
-  for (let i = 0; i < browsers.length; i++) {
-    const browser = browsers[i];
-    const match = userAgent.match(browser.regex);
-    if (match) {
-      return browser.name;
-    }
-  }
+//   // Iterate through the array to find a match
+//   for (let i = 0; i < browsers.length; i++) {
+//     const browser = browsers[i];
+//     const match = userAgent.match(browser.regex);
+//     if (match) {
+//       return browser.name;
+//     }
+//   }
 
-  // If no match is found, return "Unknown"
-  return 'Unknown';
-}
+//   // If no match is found, return "Unknown"
+//   return 'Unknown';
+// }
 
 // Get and display the browser name and version
 
-const getDesiredVoice = (browserInfo: string) => {
-  console.log(browserInfo);
+// const getDesiredVoice = (browserInfo: string) => {
+//   console.log(browserInfo);
 
-  switch (browserInfo) {
-    case 'Chrome':
-      return 'Microsoft Christopher Online (Natural) - English (United States)';
-    case 'Edge':
-      return 'Microsoft Christopher Online (Natural) - English (United States)';
-    case 'Safari':
-      return 'Flo';
-    default:
-      console.log('currently tts is not supporting this browser.');
-      return '';
-  }
-};
+//   switch (browserInfo) {
+//     case 'Chrome':
+//       return 'Microsoft Christopher Online (Natural) - English (United States)';
+//     case 'Edge':
+//       return 'Microsoft Christopher Online (Natural) - English (United States)';
+//     case 'Safari':
+//       return 'Flo';
+//     default:
+//       console.log('currently tts is not supporting this browser.');
+//       return '';
+//   }
+// };
+
 changeVoice();
 
 synth.onvoiceschanged = () => {
@@ -85,7 +86,9 @@ function changeVoice() {
         (voice) => voice.name === 'Microsoft David - English (United States)'
       );
       if (voice === undefined) {
-        voice = voices.find((voice) => voice.lang === 'en-US');
+        voice = voices.find(
+          (voice) => voice.name === 'Albert' && voice.lang === 'en-US'
+        );
       }
     }
   }
@@ -96,6 +99,7 @@ function changeVoice() {
     console.log('cannot set voice');
   }
 }
+
 export const playText = (text: string) => {
   utterance.text = text;
   synth.speak(utterance);
